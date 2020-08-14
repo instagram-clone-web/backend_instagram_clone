af2db7ee379f20415044efce9b3940ef3d39b789
matheusleal-code <alanwalkker.123@gmail.com>
2020-08-13 23:19:04 -0300

    Create post method

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 /* import dependencies */
 const   express = require('express'),
         bodyParser = require('express'),
-        mongo = require('mongodb');
+        mongo = require('mongodb'),
+        expressValidator = require('express-validator');
 
 const app = express();
 
@@ -9,8 +10,47 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+/* config middleware express-validator */
+app.use(expressValidator());
+
 /* listen port 8080 */
 const PORT = 8080;
 app.listen(PORT, function(){
     console.log(`server listening to the port ${PORT}`);
 });
+
+let db = new mongo.Db(
+    'instagram',
+    new mongo.Server('localhost', 27017, {}),
+    {}
+);
+
+app.post('/api', function(req, res){
+
+    let data = req.body;
+
+    req.assert('title', 'Title is required').notEmpty();
+    req.assert('url_image', 'Url Image is required').notEmpty();
+
+    let errors = req.validationErrors();
+    if(errors){
+        res.json(errors);
+        return;
+    }
+
+    db.open(function(err, mongoclient){
+        mongoclient.collection('posts', function(err, collection){
+            collection.insert(data, function(err, records){
+                if(err){
+                    res.json(err);
+                }else{
+                    res.json(records);
+                }
+                mongoclient.close();
+            });
+        })
+    })
+
+})
+
+
